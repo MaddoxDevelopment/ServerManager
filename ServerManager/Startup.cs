8c4a52d1cbc300f20031cc1643ae72c044129952
MaddoxDevelopment <jm@madev.me>
2019-05-08 19:02:22 -0500

    cleanup, add app insights.

--- ServerManager/Startup.cs
+++ ServerManager/Startup.cs
@@ -40,16 +40,6 @@ namespace ServerManager
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
